e944521422c3fb216d7e2054323c06e69c9e5c4f
Brian Montana <contact@brianmontana.net>
2016-05-31 08:45:32 -0500

    Update: Moved to gulp-clean-css and changes to browserSync to manually open site. Crashes when automatically opens in Windows.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -96,7 +96,7 @@ gulp.task('styles', () => {
     .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
     .pipe(gulp.dest('.tmp'))
     // Concatenate and minify styles
-    .pipe($.if('*.css', $.minifyCss()))
+    .pipe($.if('*.css', $.cleanCss({compatibility: 'ie8'})))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('dist'))
     .pipe($.size({title: 'styles'}));
@@ -143,22 +143,17 @@ gulp.task('critical', () => {
 gulp.task('clean', cb => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}, cb));
 
 // Watch files for changes & reload
-gulp.task('serve', ['styles'], () => {
-  browserSync({
-    notify: false,
-    // Customize the BrowserSync console logging prefix
-    logPrefix: 'WSK',
-    // Run as an https by uncommenting 'https: true'
-    // Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.
-    // https: true,
-    server: ['.tmp', 'app']
+gulp.task('serve', () => {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    },
+    open: false
   });
-
-  gulp.watch(['app/**/*.html'], reload);
-  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
-  gulp.watch(['app/scripts/**/*.js'], ['jshint']);
-  gulp.watch(['app/images/**/*'], reload);
+  gulp.watch('app/scripts/*.js', ['jshint', 'scripts']);
+  gulp.watch('app/styles/*.css', ['default']);
+  gulp.watch('app/*.html', ['default']);
+  gulp.watch('app/images/**/*.{svg,png,jpg,gif}', ['images']);
 });
 
 // Build and serve the output from the dist build
